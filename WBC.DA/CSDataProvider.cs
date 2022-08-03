19fe5ca2ac006075a7cd289012b3d40aa5a15d74
Otokiti <toemot2006@yahoo.co.uk>
2022-08-03 09:35:20 +0200

    Added Data Access

---
+++ WBC.DA/CSDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WBC.DA.Model;
+
+namespace WBC.DA
+{
+    public class CSDataProvider
+    {
+        public IEnumerable<CoffeeS> LoadCoffeeShop()
+        {
+            yield return new CoffeeS { Location = "CapeTown", BeansInStockInKg = 107 };
+            yield return new CoffeeS { Location = "Joburg", BeansInStockInKg = 23 };
+            yield return new CoffeeS { Location = "Pretoria", BeansInStockInKg = 56 };
+        }
+    }
+}
---
+++ WBC.DA/Model/CoffeeS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBC.DA.Model
+{
+    public class CoffeeS
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
