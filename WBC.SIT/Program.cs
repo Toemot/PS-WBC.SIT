48e0f324823ab28ec4ed732b666d648b8da46aa0
Otokiti <toemot2006@yahoo.co.uk>
2022-08-03 10:29:49 +0200

    Implement quit command

--- WBC.SIT/Program.cs
+++ WBC.SIT/Program.cs
@@ -2,13 +2,19 @@
 using WBC.DA;
 
 Console.WriteLine("Hello, Coffee Shop!");
-Console.WriteLine("Write 'help' to list the available commands");
+Console.WriteLine("Write 'help' to list the available commands, write 'quit' to exit the application.");
 
 var coffeeShopDP = new CSDataProvider();
 
 while (true)
 {
     var line = Console.ReadLine();
+
+    if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+
     var coffeeShops = coffeeShopDP.LoadCoffeeShop();
 
     if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
